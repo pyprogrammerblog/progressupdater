40ead8e00da40f9923b9154e9b02c852f6679d15
jose <pyprogrammer88@gmail.com>
2019-08-09 16:35:28 +0200

    Update

---
+++ myapp/tasks.py
@@ -0,0 +1,68 @@
+import time
+from updater.updater import ProgressUpdater, progress_updater
+from celery import task, shared_task
+
+
+
+@task()
+def current_task(**kwargs):
+    # some long code
+    time.sleep(1)
+    # more code
+
+
+@task()
+def new_format_task():
+    updater = ProgressUpdater(verbose=1)
+
+    with updater():  # if no task_name is passed, the name of the celery task is taken
+        # here the code
+        time.sleep(1)
+        # more code
+
+    updater.raise_latest_exception()  # in case of it is needed...
+
+
+@task()
+def new_format_task_1():
+    updater = ProgressUpdater(verbose=True)
+
+    with updater(task_name='TEST - First part in new format task'):
+        # here some code
+        time.sleep(1)
+        updater.notify('Some notification related to the task')
+
+    with updater(task_name='TEST - Second part in new format task'):
+        time.sleep(20)
+        my_task_2(updater=updater)
+
+    updater.raise_latest_exception()  # in case of...
+
+
+@task()
+def my_task_2(updater=None):
+    updater = updater or ProgressUpdater(verbose=True)
+
+    with updater(task_name='My subtask 1 in my task 2'):
+        # here some code
+        time.sleep(1)
+        updater.notify('Some notification related to the task')
+
+    with updater(task_name='My subtask 2 in my task 2'):
+        time.sleep(10)
+        1/0  # raise exception
+        updater.notify('Some notification related to the task')
+
+    with updater(task_name='My subtask 3 in my task 2'):
+        # here some code
+        time.sleep(1)
+        updater.notify('Some notification related to the task')
+
+
+@task()
+@progress_updater(task_name='Test 3')
+def new_format_task_3():
+    # here the code
+    time.sleep(3)
+    1/0
+    # more code
